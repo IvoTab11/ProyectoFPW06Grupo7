bf8b0f2fca4d556556c2ee08b92c829e4c769cb4
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-10-28 21:50:39 -0300

    cambios en Anotador.js. Ivo Tabarcache

--- src/components/Anotador.js
+++ src/components/Anotador.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 
 function Anotador() {
-    const [nota, setNota] = useState("");
+    const notaInicial = {
+        id: 1,
+        descripcion: "",
+        estado: true
+    };
+    const [nota, setNota] = useState(notaInicial);
     const [notas, setNotas] = useState([]);
 
     const guardarNotas = () => {
-        if (nota !== "") {
-            
-            setNotas([...notas, nota]);
+        if (nota.descripcion.trim() !== "") {
+            console.log("ID:", nota.id);
+            console.log("Descripción:", nota.descripcion);
+            console.log("Estado:", nota.estado);
 
-           
-            setNota("");
+            setNotas([...notas, nota]);
+            setNota({
+                id: nota.id + 1,
+                descripcion: "",
+                estado: true
+            });
 
             document.getElementById("valorNota").focus();
         }
     }
- 
+
     const eliminarNota = (index) => {
-        const nuevasNotas = [...notas];// decimos que la nueva const va a ser igual al arreglo de notas
+        const nuevasNotas = [...notas];
         nuevasNotas.splice(index, 1);
         setNotas(nuevasNotas);
     }
@@ -27,15 +37,15 @@ function Anotador() {
             <h2>Notas</h2>
             <input
                 type="text"
-               id="valorNota"
-                value={nota}
-                onChange={(e) => setNota(e.target.value)}
+                id="valorNota"
+                value={nota.descripcion}
+                onChange={(e) => setNota({ ...nota, descripcion: e.target.value })}
             />
             <button onClick={guardarNotas}>Anotar</button>
             <ul>
                 {notas.map((nota, index) => (
                     <li key={index}>
-                        {nota}
+                        {nota.descripcion}
                         <button onClick={() => eliminarNota(index)}>Eliminar Nota</button>
                     </li>
                 ))}
