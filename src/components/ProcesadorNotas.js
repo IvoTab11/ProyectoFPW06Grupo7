05280749c64299453167d750c770ba4ce9f11c1c
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-17 18:57:13 -0300

    Se mejoró el css en todos los componentes. Ivo Tabarcache

--- src/components/ProcesadorNotas.js
+++ src/components/ProcesadorNotas.js
@@ -1,45 +1,10 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {Row} from 'react-bootstrap';
 
-const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: 'darkgray',
-    color: 'white', // Cambia el color de la letra a rojo
-  };
-  
-  const inputStyle = {
-    fontSize: '24px',
-    padding: '10px',
-    width: '80%',
-    margin: '10px',
-    textAlign: 'center',
-  };
-  
-  const ulStyle = {
-    listStyle: 'none',
-    padding: '0',
-  };
-  
-  const listItemStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: 'darkgray',
-    padding: '8px',
-    margin: '4px',
-    fontSize: '40px',
-    color: 'black'
-  };
 function ProcesadorNotas({notasEnProceso,eliminarNotaProceso,resolverNota}){
     return (
-        <ul style={ulStyle}>
-               <Row>
+        <ul style={{justifyContent: 'center', alignItems: 'center'}}>
                 {notasEnProceso.map((nota, index) => (
                     <Card key={index} style={{ width: '18rem' }}>
                     <Card.Body>
@@ -52,7 +17,6 @@ function ProcesadorNotas({notasEnProceso,eliminarNotaProceso,resolverNota}){
                     </Card.Body>
                   </Card>
                 ))}
-                </Row> 
             </ul>
     );
 }
